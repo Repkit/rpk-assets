49b3ce3f28168bb747ae395d17a0f7bccd47f719
repkit <ionut.baches@yahoo.com>
2016-07-11 19:29:05 +0000

    ambilight video

---
+++ by-mistake/js/home.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var ambilight;
+var video;
+function onYouTubeIframeAPIReady() {
+  ambilight = new YT.Player('ambilight', {
+    videoId: 'uDpojAGsthw',
+    events: {
+      'onReady': ambilightReady
+    }
+  });
+  video = new YT.Player('video', {
+    videoId: 'uDpojAGsthw',
+    events: {
+      'onReady': videoReady,
+      'onStateChange': stateChange
+    }
+  });
+}
+
+function ambilightReady(event) {
+  ambilight.mute();
+}
+
+function videoReady(event) {
+}
+
+function stateChange(state){
+  if(state.data == 1){
+    setTimeout(function(){ 
+      ambilight.seekTo(1);
+      ambilight.playVideo();
+    }, 500);
+  }else if(state.data == 2){
+    ambilight.pauseVideo();
+  }
+}
